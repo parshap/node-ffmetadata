1a206beec7e68cb8bbd3b4090f50a658e10473ed
Parsha Pourkhomami <parshap+git@gmail.com>
2013-08-01 09:51:49 +0000

    Test reading metadata from mp3

---
+++ test/test.js
@@ -0,0 +1,20 @@
+/* jshint node:true */
+"use strict";
+
+var path = require("path"),
+	fs = require("fs"),
+	test = require("tape"),
+	ffmetadata = require("../");
+
+var TEST_FILE = path.join(__dirname, "test.mp3");
+
+test("read metadata", function(t) {
+	fs.createReadStream(TEST_FILE)
+		.pipe(ffmetadata(function(data) {
+			t.ok(data);
+			t.ok(data.artist);
+			t.ok(data.disc);
+			t.ok(data.encoded_by);
+			t.end();
+		}));
+});
